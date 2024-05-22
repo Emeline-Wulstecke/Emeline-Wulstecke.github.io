e1e1578adf79a20cfb3d8ef0ba0823be4ef73cc1
emelinew1 <emeline.wulstecke@gmail.com>
2024-05-22 14:42:06 +0200

    Optimisation img

--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -2,24 +2,27 @@ import './footer.css';
 import Link from '../../atoms/link/Link';
 import data from '../../../assets/data.json';
 
-
-
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer id='footer'>
-            <h3> Emeline Wulstecke</h3>
+            <h3>Emeline Wulstecke</h3>
             <ul>
-                <li><a href={`mailto:${data.email.url}`}><i className={data.email.icon}></i></a></li>
+                <li>
+                    <a href={`mailto:${data.email.url}`} aria-label={`Envoyer un email à ${data.email.name}`}>
+                        <i className={data.email.icon}></i>
+                    </a>
+                </li>
                 {data.links.map((link, index) => (
-                    <li key={index}><Link url={link.url} content={
-                        <>
-                          <i className={link.icon}></i>
-                          <span className="visually-hidden">{link.name}</span>
-                        </>
-                      } aria-label={link.name}/></li>
+                    <li key={index}>
+                        <Link url={link.url} content={
+                            <>
+                                <i className={link.icon}></i>
+                                <span>{link.name}</span>
+                            </>
+                        } aria-label={link.name} />
+                    </li>
                 ))}
-
             </ul>
             <p>© {currentYear} || Tous droits réservés</p>
         </footer>
